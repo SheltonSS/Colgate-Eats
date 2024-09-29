72da8370ae9c2801e4a02a951317cb41eded13ed
Algaram <ieblazed40@gmail.com>
2024-09-29 02:49:38 -0400

    Added food suggestion page
    Added food suggestion page to show generated suggestions from the preference sliders

---
+++ colgateeats/src/Components/FoodSuggestions.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+const FoodSuggestions = ({ route }) => {
+  return (
+    <Box sx={{ width: '80%', margin: 'auto', textAlign: 'center' }}>
+      <Typography variant="h4" gutterBottom>
+        Food Suggestions Based on Your Preferences
+      </Typography>
+      <Typography variant="body1">
+        Based on the predicted route: <strong>{route}</strong>, here are some food suggestions:
+      </Typography>
+
+      {/* This is where you'd list the suggestions; for now, we'll mock some data */}
+      <Box sx={{ marginTop: 4 }}>
+        <Typography variant="h6">Suggested Dishes:</Typography>
+        <ul>
+          <li>Light & Sweet Salad</li>
+          <li>Mild Vegetarian Stir-fry</li>
+          <li>Western-style Light Pasta</li>
+          {/* More suggestions based on route */}
+        </ul>
+      </Box>
+    </Box>
+  );
+};
+
+export default FoodSuggestions;
